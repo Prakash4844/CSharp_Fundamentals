b62cd25cae71235640629354a91d524862bf08ae
Prakash4844 <pk484442@gmail.com>
2022-05-11 21:12:14 +0530

    Updated gitignore to ignore json and csproj files and Restructured The Repository

---
+++ src/2_Data_Type_and_Variable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeAndVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This File shows the use of Data Type And Variable.");
+        }
+    }
+}
