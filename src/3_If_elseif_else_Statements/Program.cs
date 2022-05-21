39e0b016787f9959d5c82136cc94f2272aa3a3e9
Prakash4844 <pk484442@gmail.com>
2022-05-21 19:17:18 +0530

    Added Src Project 3 If Else if

---
+++ src/3_If_elseif_else_Statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "If Else Tutorial")); //write in centre of console.
+
+            Console.WriteLine("A Game");
+
+            Console.WriteLine("Choose a Door: A, B And C:");
+            string? userValue = Console.ReadLine();
+
+
+            if (userValue == "A") //If statement are called decision statements it will execute a code inside a if statement only if a condition is satisfied.
+                                    //otherwise the code inside if statement is ignored
+            {
+                string message = "You Get a Car!";
+                Console.WriteLine(message);
+
+            }
+        }
+    }
+}
