6de784f0702b94be5194d5af4f964b86651068c4
Prakash4844 <pk484442@gmail.com>
2022-07-21 21:22:27 +0530

    Added 20_Events, 20_WPFEvents

---
+++ src/20_Events/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Timers;
+
+namespace TimerEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Events are user actions such as key press, clicks, mouse movements, etc., or some occurrence such as 
+                system generated notifications. Applications need to respond to events when they occur. 
+                For example, interrupts. Events are used for inter-process communication.
+
+            Events are user actions such as key press, clicks, mouse movements, etc., 
+            or some occurrence such as system generated notifications. 
+            Applications need to respond to events when they occur. For example, interrupts. 
+            Events are used for inter-process communication.
+             */
+            Console.Clear();
+            Console.WriteLine(string.Format("{0, " + Console.WindowWidth / 2 + "}", "Event Tutorial"));//Print in middle of console.
+
+            System.Timers.Timer MyTimer = new System.Timers.Timer(2000);
+
+            MyTimer.Elapsed += MyTimer_Elapsed;
+            MyTimer.Elapsed += MyTimer_Elapsed1;
+
+            MyTimer.Start();
+
+            Console.WriteLine("Press Enter to terminate Red Event");
+            Console.ReadLine();
+
+            MyTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object? sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0:HH:mm:ss.fff}", e.SignalTime);
+
+        }
+    }
+}
---
+++ src/20_WPFEvents/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20_WPFEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ORA_Button.Click += Ora_Click;
+            
+        }
+
+        private void Ora_Click(object sender, RoutedEventArgs e)
+        {
+            MyOtherLabel.Content = "ORAAA! ORA! ORA! ORA! ORAAA!";
+        }
+
+        private void ORA_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyLabel.Content = "ORA! ORA! ORA! ORA! ORA!";
+        }
+    }
+}
